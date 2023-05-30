eed1aaccd0c854bbad03f5c33877b921d16099ce
tadc <72883077+cdat1904@users.noreply.github.com>
2023-05-30 21:15:38 +0700

    Add files via upload

---
+++ Code C/B7_Ex2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void reverse(char array[],int size)
+{
+    for (int i = size; i >= 0; i--) {
+        if (array[i] == ' ') {
+            array[i] = '\0';
+            printf("%s ", &(array[i]) + 1);
+        }
+    }
+    printf("%s", array);
+}
+
+int main()
+{
+    char array[] = "mot hai ba bon nam ";
+    int size = sizeof(array) / sizeof(array[0]);
+    reverse(array, size);
+    return 0;
+}
